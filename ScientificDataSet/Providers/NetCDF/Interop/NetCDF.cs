5e1990906cec66b1f7b111f5bc5a4c82de3a5d69
Vassily Lyutsarev <vassilyl@microsoft.com>
2021-01-20 15:04:00 +0000

    Amend netcdflib search on Windows

--- ScientificDataSet/Providers/NetCDF/Interop/NetCDF.cs
+++ ScientificDataSet/Providers/NetCDF/Interop/NetCDF.cs
@@ -548,21 +548,34 @@ namespace NetCDFInterop
                     if (null == path)
                     {
                         // alternatively try standard install paths.
-                        var ncdir = Directory.GetDirectories(Environment.GetFolderPath(
-                            Environment.Is64BitProcess ? Environment.SpecialFolder.ProgramFiles : Environment.SpecialFolder.ProgramFilesX86))
-                            .Reverse() // last version first
-                            .Where(d => 0 < d.IndexOf("netcdf", StringComparison.InvariantCultureIgnoreCase))
-                            .Select(d => Path.Combine(d, "bin"))
-                            .FirstOrDefault(d => File.Exists(Path.Combine(d, name)));
-                        if (null != ncdir)
+                        var programFiles = Environment.GetFolderPath(
+                            Environment.Is64BitProcess || !Environment.Is64BitOperatingSystem 
+                            ? Environment.SpecialFolder.ProgramFiles 
+                            : Environment.SpecialFolder.ProgramFilesX86);
+                        if (string.Empty == programFiles)
                         {
-                            // if found we need to add the file location to PATH environmental variable to load dependent DLLs.
-                            path = ncdir;
-                            Environment.SetEnvironmentVariable("PATH",
-                                Environment.GetEnvironmentVariable("PATH") + ";" + ncdir);
+                            // In windows containers `GetFolderPath` returns empty string
+                            programFiles = Environment.GetEnvironmentVariable(
+                            Environment.Is64BitProcess || !Environment.Is64BitOperatingSystem
+                            ? "ProgramFiles" : "ProgramFiles(x86)");
+                        }
+                        if (!string.IsNullOrWhiteSpace(programFiles))
+                        {
+                            var ncdir = Directory.GetDirectories(programFiles)
+                                .Reverse() // last version first
+                                .Where(d => 0 < d.IndexOf("netcdf", StringComparison.InvariantCultureIgnoreCase))
+                                .Select(d => Path.Combine(d, "bin"))
+                                .FirstOrDefault(d => File.Exists(Path.Combine(d, name)));
+                            if (null != ncdir)
+                            {
+                                // if found we need to add the file location to PATH environmental variable to load dependent DLLs.
+                                path = ncdir;
+                                Environment.SetEnvironmentVariable("PATH",
+                                    Environment.GetEnvironmentVariable("PATH") + ";" + ncdir);
+                            }
                         }
                     }
-                    if (null == path) throw new FileNotFoundException(name + " not found in current directory nor on system path nor in ProgramFilesX86");
+                    if (null == path) throw new FileNotFoundException(name + " not found in current directory nor on system path nor in Program Files directories.");
                     return Path.Combine(path, name);
                 case PlatformID.Unix:
                     return "/usr/lib/libnetcdf.so.7";
