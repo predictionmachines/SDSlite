ddff5c0b7c0ef54644e3b635d07b36f0dbb90c0b
Vassily Lyutsarev <vassilyl@users.noreply.github.com>
2021-01-21 00:00:17 +0000

    [2.0.2] Support reading netCDF variables of type NC_CHAR (#29)
    netCDF NC_CHAR type is now mapped to C# `byte` instead of `sbyte`. The ScientificDataSet library still cannot create netCDF variables of type NC_CHAR, but successfully reads/writes such variables in existing `.nc` files as `byte` arrays. 
    
    * Create a failing repro for the issue
    
    * Fix the bug
    
    * Update version to 2.0.2

---
+++ SDSLiteTests/Repro_28.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Research.Science.Data;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDSLiteTests
+{
+    public class Repro_28
+    {
+        [Test]
+        public void CanReadNetcdfNcChar()
+        {
+            var ncpath = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString("N") + ".nc");
+            File.WriteAllBytes(ncpath, Convert.FromBase64String(netcdf_content));
+            try
+            {
+                using var ds = DataSet.Open(ncpath, ResourceOpenMode.Open);
+                Assert.AreEqual(1, ds.Variables.Count);
+                Assert.AreEqual(2, ds.Dimensions.Count);
+                Assert.AreEqual("test", ds.Variables[0].Name);
+                Assert.AreEqual("count", ds.Dimensions[0].Name);
+                Assert.AreEqual(1, ds.Dimensions[0].Length);
+                Assert.AreEqual("size", ds.Dimensions[1].Name);
+                Assert.AreEqual(8, ds.Dimensions[1].Length);
+                var data = ds[0].GetData();
+                Assert.AreEqual(2, data.Rank);
+                Assert.AreEqual("value   ", Encoding.UTF8.GetString((data.Cast<byte>().ToArray())));
+                ds[0].Append(Encoding.UTF8.GetBytes("addition"));
+                Assert.AreEqual(2, ds.Dimensions[0].Length);
+                data = ds[0].GetData();
+                Assert.AreEqual("value   addition", Encoding.UTF8.GetString((data.Cast<byte>().ToArray())));
+            }
+            finally
+            {
+                File.Delete(ncpath);
+            }
+        }
+        // [1] test of type SByte (count:1) (size:8)
+        const string netcdf_content = @"
+Q0RGAQAAAAEAAAAKAAAAAgAAAARzaXplAAAACAAAAAVjb3VudAAAAAAAAAAAAAAAAAAAAAAAAAsA
+AAABAAAABHRlc3QAAAACAAAAAQAAAAAAAAAAAAAAAAAAAAIAAAAIAAAAZHZhbHVlICAg";
+    }
+}
